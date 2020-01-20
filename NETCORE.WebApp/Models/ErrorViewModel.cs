0906c92307e09b8c1876ba7a47fa556d7b4d7cf1
quan le <trngqu4n2110@outlook.com>
2020-01-20 20:39:17 +0700

    Initial solution and repository

---
+++ NETCORE.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NETCORE.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
